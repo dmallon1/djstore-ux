8c7cc8adf9444dfe44e1e1d6ae6aef4a46bea41e
Dan Mallon <d.mallon@me.com>
2019-12-15 13:16:38 -0500

    Improved front end validation

--- src/app/Stripe.js
+++ src/app/Stripe.js
@@ -4,6 +4,7 @@ import {Link} from "react-router-dom";
 import {Checkout} from './Checkout';
 import {Order} from "./Order";
 import {states, postData} from "./utils";
+import Alert from 'react-bootstrap/Alert'
 
 
 export function Stripe(props) {
@@ -24,8 +25,11 @@ class _CardForm extends React.Component {
         this.state = {
             doesAgree: false,
             order: new Order(),
+            errorMsg: null,
         };
     }
+
+    closeMsg = () => this.setState({errorMsg: null});
 
     handleChange(key, event) {
         let order = this.state.order;
@@ -48,11 +52,10 @@ class _CardForm extends React.Component {
         if (this.props.stripe) {
             this.props.stripe.createToken().then(payload => {
                 if (payload.error) {
-                    // write error somewhere
-                    console.log(payload.error);
+                    // could do more this this error payload
+                    this.setState({errorMsg: payload.error.message})
                     return;
                 }
-                console.log(payload);
                 let orderInfo = this.state.order;
                 orderInfo.card_token = payload.token.id;
                 orderInfo.captcha_token = 1;
@@ -100,20 +103,21 @@ class _CardForm extends React.Component {
                 <h1 className="font-weight-light">checkout</h1>
                 <hr className="border border-dark m-0 mb-4"/>
                 <Checkout {...this.props}/>
-                <input className="my-2 mr-2" style={{width:100+'%'}} type="text" placeholder="email" value={this.state.order.email || ''} onChange={(e) => this.handleChange('email', e)}/>
+                <input required className="my-2 mr-2" style={{width:100+'%'}} type="email" placeholder="email" value={this.state.order.email || ''} onChange={(e) => this.handleChange('email', e)}/>
                 {/* Name and address */}
                 <div className="d-flex justify-content-between">
-                    <input className="my-2 mr-2" style={{width:100+'%'}} type="text" placeholder="first name" value={this.state.order.first_name || ''} onChange={(e) => this.handleChange('first_name', e)}/>
-                    <input className="my-2 ml-2" style={{width:100+'%'}} type="text" placeholder="last name" value={this.state.order.last_name || ''} onChange={(e) => this.handleChange('last_name', e)}/>
+                    <input required className="my-2 mr-2" style={{width:100+'%'}} type="text" placeholder="first name" value={this.state.order.first_name || ''} onChange={(e) => this.handleChange('first_name', e)}/>
+                    <input required className="my-2 ml-2" style={{width:100+'%'}} type="text" placeholder="last name" value={this.state.order.last_name || ''} onChange={(e) => this.handleChange('last_name', e)}/>
                 </div>
-                <input className="my-2" style={{width:100+'%'}} type="text" placeholder="address" value={this.state.order.address1 || ''} onChange={(e) => this.handleChange('address1', e)}/>
+                <input required className="my-2" style={{width:100+'%'}} type="text" placeholder="address" value={this.state.order.address1 || ''} onChange={(e) => this.handleChange('address1', e)}/>
                 <div className="d-flex justify-content-between">
                     <input className="my-2 mr-2" style={{width:60+'%'}} type="text" placeholder="apt, unit, etc" value={this.state.order.address2 || ''} onChange={(e) => this.handleChange('address2', e)}/>
-                    <input className="my-2" style={{width:40+'%'}} type="text" placeholder="zip code" value={this.state.order.zip || ''} onChange={(e) => this.handleChange('zip', e)}/>
+                    <input required className="my-2" style={{width:40+'%'}} type="text" placeholder="zip code" value={this.state.order.zip || ''} onChange={(e) => this.handleChange('zip', e)}/>
                 </div>
                 <div className="d-flex justify-content-between">
-                    <input className="my-2 mr-2" style={{width:80+'%'}} type="text" placeholder="city" value={this.state.order.city || ''} onChange={(e) => this.handleChange('city', e)}/>
-                    <select name="state" className="my-2" value={this.state.order.state} onChange={(e) => this.handleChange('state', e)} style={{height:'28px'}}>
+                    <input required className="my-2 mr-2" style={{width:80+'%'}} type="text" placeholder="city" value={this.state.order.city || ''} onChange={(e) => this.handleChange('city', e)}/>
+                    <select required name="state" className="my-2" value={this.state.order.state || ""} onChange={(e) => this.handleChange('state', e)} style={{height:'28px'}}>
+                        <option disabled hidden value="">select state</option>
                         {states.map((state,i) => (
                             <option key={i} value={state}>{state}</option>
                         ))}
@@ -124,6 +128,11 @@ class _CardForm extends React.Component {
                     <hr className="border border-dark m-3" style={{width:200+'px'}}/>
                 </div>
                 {/* Stripe */}
+                {this.state.errorMsg &&
+                    <Alert className="m-0" variant="danger" onClose={() => this.closeMsg()} dismissible>
+                        {this.state.errorMsg}
+                    </Alert>
+                }
                 <CardElement
                     onBlur={handleBlur}
                     onChange={handleChange}
@@ -131,10 +140,11 @@ class _CardForm extends React.Component {
                     onReady={handleReady}
                     {...createOptions(this.props.fontSize)}
                 />
+                <div className="float-right" style={{fontSize: '10px'}}>payments handled by <a href="https://stripe.com/" target="_blank" rel="noopener noreferrer">stripe</a></div><br/>
                 {/* Checkbox */}
                 <div className="car d-flex">
                     <label>
-                        <input style={{width:25+'px', height:25+'px'}}
+                        <input required style={{width:25+'px', height:25+'px'}}
                             name="doesAgree"
                             type="checkbox"
                             checked={this.state.doesAgree}
